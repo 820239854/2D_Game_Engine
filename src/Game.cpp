bbf28e79b3aed3353dce2c8d69b7c41d7a4a6a35
Archer <820239854@qq.com>
2023-04-29 14:48:53 +0800

    Capping the Game Framerate

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 Game::Game()
@@ -68,14 +69,26 @@ void Game::ProcessInput()
     }
 }
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
 void Game::Setup()
 {
-    // TODO: Initialize game objects...
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(0.5, 0.0);
 }
 
 void Game::Update()
 {
-    // TODO: Update game objects...
+    // If we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME))
+        ;
+
+    // Store the "previous" frame time
+    millisecsPreviousFrame = SDL_GetTicks();
+
+    playerPosition.x += playerVelocity.x;
+    playerPosition.y += playerVelocity.y;
 }
 
 void Game::Render()
@@ -89,7 +102,11 @@ void Game::Render()
     SDL_FreeSurface(surface);
 
     // What is the destination rectangle that we want to place our texture
-    SDL_Rect dstRect = {10, 10, 32, 32};
+    SDL_Rect dstRect = {
+        static_cast<int>(playerPosition.x),
+        static_cast<int>(playerPosition.y),
+        32,
+        32};
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
     SDL_DestroyTexture(texture);
 
