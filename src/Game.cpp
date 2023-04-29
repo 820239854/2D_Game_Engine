0d59db33b9f3c64c4e2c6520a459e83070d67bd7
Archer <820239854@qq.com>
2023-04-29 14:38:25 +0800

    全屏设置

--- src/Game.cpp
+++ src/Game.cpp
@@ -20,25 +20,30 @@ void Game::Initialize()
         std::cerr << "Error initializing SDL." << std::endl;
         return;
     }
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    windowWidth = 800;
+    windowHeight = 600;
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        windowWidth,
+        windowHeight,
         SDL_WINDOW_BORDERLESS);
     if (!window)
     {
         std::cerr << "Error creating SDL window." << std::endl;
         return;
     }
-    renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer)
     {
         std::cerr << "Error creating SDL renderer." << std::endl;
         return;
     }
-
+    // 在保留原比例情况下，将窗口缩放到最大 (最大宽度或最大高度)
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
     isRunning = true;
 }
 
@@ -62,6 +67,11 @@ void Game::ProcessInput()
     }
 }
 
+void Game::Setup()
+{
+    // TODO: Initialize game objects...
+}
+
 void Game::Update()
 {
     // TODO: Update game objects...
@@ -72,13 +82,14 @@ void Game::Render()
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    // TODO: Render all game objects...
+    // TODO: Draw our game objects
 
     SDL_RenderPresent(renderer);
 }
 
 void Game::Run()
 {
+    Setup();
     while (isRunning)
     {
         ProcessInput();
