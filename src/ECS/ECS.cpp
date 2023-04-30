a430eb53ae274cafbf65affb6a2428816b5c6f73
Archer <820239854@qq.com>
2023-04-30 15:46:00 +0800

    ECS Implementation

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,5 +1,8 @@
 #include <algorithm>
 #include "ECS.h"
+#include "../Logger/Logger.h"
+
+int IComponent::nextId = 0;
 
 void System::AddEntity(Entity entity)
 {
@@ -24,4 +27,36 @@ std::vector<Entity> System::GetEntities() const
 const Signature System::GetComponentsSignature() const
 {
     return componentSignature;
+}
+
+Entity Registry::CreateEntity()
+{
+    int entityId = numEntities++;
+    if (entityId >= entityComponentsSignatures.size())
+    {
+        entityComponentsSignatures.resize(entityId + 1);
+    }
+    Entity entity(entityId);
+    entitiesToCreate.insert(entity);
+
+    Logger::Log("Entity created with id: " + std::to_string(entityId));
+    return entity;
+}
+
+void Registry::Update()
+{
+}
+
+void Registry::AddEntityToSystems(Entity entity)
+{
+    const auto entityId = entity.GetId();
+    const auto entitySignature = entityComponentsSignatures[entityId];
+    for (auto &system : systems)
+    {
+        const auto &systemSignature = system.second->GetComponentsSignature();
+        if ((entitySignature & systemSignature) == systemSignature)
+        {
+            system.second->AddEntity(entity);
+        }
+    }
 }
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -2,6 +2,9 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
+#include <set>
 
 const unsigned int MAX_COMPONENTS = 32;
 typedef std::bitset<MAX_COMPONENTS> Signature;
@@ -155,9 +158,38 @@ public:
     Registry() = default;
     ~Registry() = default;
 
+    Entity CreateEntity();
+    void KillEntity(Entity entity);
+    void Update();
+    void AddEntityToSystem(Entity entity, System *system);
+    void RemoveEntityFromSystem(Entity entity, System *system);
+
+    template <typename TComponrnt, typename... TArgs>
+    void AddComponent(Entity entity, TArgs &&...args);
+    template <typename TComponrnt>
+    void RemoveComponent(Entity entity);
+    template <typename TComponrnt>
+    bool HasComponent(Entity entity) const;
+
+    template <typename TSystem, typename... TArgs>
+    void AddSystem(TArgs &&...args);
+    template <typename TSystem>
+    void RemoveSystem();
+    template <typename TSystem>
+    bool HasSystem() const;
+    template <typename TSystem>
+    TSystem &GetSystem() const;
+
+    void AddEntityToSystems(Entity entity);
+
 private:
     int numEntities;
     std::vector<IPool *> componentPools;
+    std::vector<Signature> entityComponentsSignatures;
+    std::unordered_map<std::type_index, System *> systems;
+
+    std::set<Entity> entitiesToCreate;
+    std::set<Entity> entitiesToKill;
 };
 
 template <typename TComponrnt>
@@ -166,3 +198,74 @@ void System::RequireComponent()
     const auto componentId = Component<TComponrnt>::GetId();
     componentSignature.set(componentId);
 }
+
+template <typename TComponrnt, typename... TArgs>
+void Registry::AddComponent(Entity entity, TArgs &&...args)
+{
+    const auto componentId = GetComponentId<TComponrnt>().GetId();
+    const auto entityId = entity.GetId();
+
+    if (componentId >= componentPools.size())
+    {
+        componentPools.resize(componentId + 1, nullptr);
+    }
+
+    if (!componentPools[componentId])
+    {
+        componentPools[componentId] = new Pool<TComponrnt>();
+    }
+
+    Pool<TComponrnt> *componentPool = componentPools[componentId];
+    if (entityId >= componentPool->GetSize())
+    {
+        componentPool->Resize(numEntities);
+    }
+
+    TComponrnt newComponent(std::forward<TArgs>(args)...);
+    componentPool->Set(entityId, newComponent);
+    entityComponentsSignatures[entityId].set(componentId);
+};
+
+template <typename TComponrnt>
+void Registry::RemoveComponent(Entity entity)
+{
+    const auto componentId = GetComponentId<TComponrnt>().GetId();
+    const auto entityId = entity.GetId();
+    entityComponentsSignatures[entityId].set(componentId, false);
+};
+
+template <typename TComponrnt>
+bool Registry::HasComponent(Entity entity) const
+{
+    const auto componentId = GetComponentId<TComponrnt>().GetId();
+    const auto entityId = entity.GetId();
+    return entityComponentsSignatures[entityId].test(componentId);
+};
+
+template <typename TSystem, typename... TArgs>
+void Registry::AddSystem(TArgs &&...args)
+{
+    TSystem *newSystem(new TSystem(std::forward<TArgs>(args)...));
+    systems.insert(std::make_pair(std::type_index(typeid(TSystem)), newSystem));
+};
+
+template <typename TSystem>
+void Registry::RemoveSystem()
+{
+    const auto systemId = systems.find(std::type_index(typeid(TSystem)));
+    systems.erase(systemId);
+};
+
+template <typename TSystem>
+bool Registry::HasSystem() const
+{
+    const auto systemId = systems.find(std::type_index(typeid(TSystem)));
+    return systemId != systems.end();
+};
+
+template <typename TSystem>
+TSystem &Registry::GetSystem() const
+{
+    const auto systemId = systems.find(std::type_index(typeid(TSystem)));
+    return *(static_cast<TSystem*>(systemId->second));
+};
