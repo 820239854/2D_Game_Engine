b8543fe4f69e7ab6b3f1cf93016e25a47de23193
Archer <820239854@qq.com>
2023-05-06 23:52:48 +0800

    Lua Scripting

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -5,6 +5,7 @@
 #include "../AssetStore/AssetStore.h"
 #include "../EventBus/EventBus.h"
 #include <SDL2/SDL.h>
+#include <sol/sol.hpp>
 
 const int FPS = 60;
 const int MILLISECS_PER_FRAME = 1000 / FPS;
@@ -19,6 +20,8 @@ private:
     SDL_Renderer *renderer;
     SDL_Rect camera;
 
+    sol::state lua;
+
     std::unique_ptr<Registry> registry;
     std::unique_ptr<AssetStore> assetStore;
     std::unique_ptr<EventBus> eventBus;
@@ -29,7 +32,6 @@ public:
     void Initialize();
     void Run();
     void Setup();
-    void LoadLevel(int level);
     void ProcessInput();
     void Update();
     void Render();
